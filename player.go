5cf2225b222853cbc01cf3e54bd1d2d875dc3925
akatsuki <benmo0802@gmail>
2019-10-05 12:39:50 +0800

    player modification

--- player.go
+++ player.go
@@ -3,20 +3,30 @@ package main
 import (
 	"net/http"
 	"net/url"
+	"time"
 
+	"github.com/faiface/beep"
+	"github.com/faiface/beep/effects"
 	"github.com/faiface/beep/mp3"
+	"github.com/faiface/beep/speaker"
 )
 
 type Player struct {
-	List  []*Song
-	*Song // current focus
-	Playlist
+	// music stream instance
+	MusicInstance
+
+	CurrentList
+	Playlist       []*Song
+	CurrentFocus   *Song // current focus
+	CurrentPlaying *Song // current focus
 
 	History []*Song
 
 	PlayMode
-	Volume float32
-	Speed  Speed
+	Volume float64
+	Speed  beep.SampleRate
+
+	SpeakerInitiated bool
 
 	// request to the music server
 	Loader
@@ -26,51 +36,178 @@ func NewPlayer() *Player {
 	return &Player{}
 }
 
-func (p *Player) Play(s *Song) {
-	u, err := url.ParseRequestURI(s.URL)
-	chk(err)
-	p.Req.URL = u
-	resp, err := p.Client.Do(p.Req)
-	chk(err)
-	defer resp.Body.Close()
+func (p *Player) InitSpeaker(sr beep.SampleRate, bufsize int) *Player {
+	speaker.Init(sr, bufsize)
+	return p
+}
 
-	streamer, format, err := mp3.Decode(resp.Body)
+func (p *Player) prepare(s *Song) *Player {
+	// parse song url : *url.URL
+	songurl, err := url.ParseRequestURI(s.URL)
 	chk(err)
+	// update url of request of player loader
+	p.Loader.Req.URL = songurl
+	resp, err := p.Loader.Client.Do(p.Req)
+	chk(err)
+	// decode response body which is an io.ReadCloser interfce
+	// defalt decoder mp3 -> tobe improved
+	streamer, f, err := mp3.Decode(resp.Body)
 	defer streamer.Close()
+	// check if speaker initialized
+	if !p.SpeakerInitiated {
+		p.InitSpeaker(f.SampleRate*p.Speed, f.SampleRate.N(time.Second/20))
+		p.SpeakerInitiated = true
+	}
+	// assign music instance
+	ctrl := &beep.Ctrl{Streamer: streamer, Paused: false}
+	p.MusicInstance = MusicInstance{
+		Streamer: streamer,
+		Ctrl:     ctrl,
+		Vol:      &effects.Volume{Streamer: ctrl, Base: 2, Volume: p.Volume, Silent: false},
+	}
+	return p
+}
+
+func (p *Player) Play(s *Song) {
+	// prepare
 	done := make(chan bool)
-
+	p.prepare(s)
+	defer p.CloseStreamer()
+	speaker.Play(beep.Seq(p.MusicInstance.Vol), beep.Callback(func() {
+		done <- true
+	}))
+	<-done
 }
 
-func (p *Player) Pause() {
+func (p *Player) CloseStreamer() *Player {
+	p.MusicInstance.Streamer.Close()
+	return p
 }
 
-func (p *Player) PlayFocus() *Player {
+func (p *Player) IncreaseVol() *Player {
+	p.Volume += 0.2
+	speaker.Lock()
+	p.Vol.Volume = p.Volume
+	speaker.Unlock()
+	return p
+}
+
+func (p *Player) DecreaseVol() *Player {
+	p.Volume -= 0.2
+	speaker.Lock()
+	p.Vol.Volume = p.Volume
+	speaker.Unlock()
+	return p
+}
+
+func (p *Player) Mute() *Player {
+	speaker.Lock()
+	p.Vol.Silent = true
+	speaker.Unlock()
+	return p
+}
+
+func (p *Player) Unmute() *Player {
+	speaker.Lock()
+	p.Vol.Silent = false
+	speaker.Unlock()
+	return p
+}
+
+func (p *Player) ToggleMute() *Player {
+	speaker.Lock()
+	p.Vol.Silent = !p.Vol.Silent
+	speaker.Unlock()
+	return p
+}
+
+func (p *Player) Pause() *Player {
+	speaker.Lock()
+	p.Ctrl.Paused = true
+	speaker.Unlock()
+	return p
+}
+
+func (p *Player) Resume() *Player {
+	speaker.Lock()
+	p.Ctrl.Paused = false
+	speaker.Unlock()
 	return p
 }
 
 func (p *Player) TogglePlay() *Player {
+	speaker.Lock()
+	p.Ctrl.Paused = !p.Ctrl.Paused
+	speaker.Unlock()
 	return p
 }
 
-func (p *Player) NextSong() *Player {
+func (p *Player) PlayFocus() {
+	p.Play(p.CurrentFocus)
+}
+
+func (p *Player) PrevSong() *Song {
+	l := len(p.CurrentList.Songs)
+	i := p.CurrentList.Index
+	if l < 1 {
+		return nil
+	}
+	if i == 0 {
+		return p.CurrentList.Songs[l-1]
+	}
+	return p.CurrentList.Songs[i-1]
+}
+
+func (p *Player) NextSong() *Song {
+	l := len(p.CurrentList.Songs)
+	i := p.CurrentList.Index
+	if l < 1 {
+		return nil
+	}
+	if i == l-1 {
+		return p.CurrentList.Songs[0]
+	}
+	return p.CurrentList.Songs[i+1]
+}
+
+func (p *Player) UpdateFocus() *Player {
+
 	return p
 }
 
-// func (p *Player) Pause() *Player {
-// 	return p
-// }
+func (p *Player) FocusNextSong() *Player {
+	l := len(p.CurrentList.Songs)
+	i := p.CurrentList.Index
+	if l > 0 {
+		if i == l-1 {
+			p.CurrentList.Index = 0
 
-// func (p *Player) Pause() *Player {
-// 	return p
-// }
+		} else {
+			p.CurrentList.Index = 0
+		}
+	}
+	return p
+}
 
-// func (p *Player) Pause() *Player {
-// 	return p
-// }
+func (p *Player) FocusTop() *Player {
+	l := len(p.CurrentList.Songs)
+	if l > 0 {
+		p.CurrentList.Index = 0
+	}
+	return p
+}
 
-type Playlist struct {
+func (p *Player) FocusBottom() *Player {
+	l := len(p.CurrentList.Songs)
+	if l > 0 {
+		p.CurrentList.Index = l - 1
+	}
+	return p
+}
+
+type CurrentList struct {
 	Songs []*Song
-	Index int16
+	Index int
 }
 
 type Song struct {
@@ -110,9 +247,7 @@ type SongURL struct {
 	Expire int
 }
 
-type Speed float32
-
-var speedMap = map[string]Speed{
+var speedMap = map[string]float64{
 	"1.0x": 1.00,
 	"1.2x": 1.20,
 	"1.4x": 1.40,
@@ -155,4 +290,10 @@ func NewLoader() *Loader {
 		},
 		Client: &http.Client{},
 	}
+}
+
+type MusicInstance struct {
+	Streamer beep.StreamSeekCloser
+	Ctrl     *beep.Ctrl
+	Vol      *effects.Volume
 }
